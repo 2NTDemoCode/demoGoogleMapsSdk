650257e588ea87f3f15d5a8bc2a937ae96b6a6e5
Chamroeun Chun <minea@mineas-mac.local>
2017-03-10 07:58:36 +0700

    Initial Commit

---
+++ MapGoogleAPIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapGoogleAPIDemo
+//
+//  Created by Minea Chem on 3/10/17.
+//  Copyright © 2017 Minea Chem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
